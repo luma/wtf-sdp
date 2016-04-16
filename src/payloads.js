a10ee82ddebc98caeba2dfb18a3b775491b380cb
Rolly Fordham <rolly@luma.co.nz>
2016-04-16 14:08:27 +1000

    add payload specs

--- src/payloads.js
+++ src/payloads.js
@@ -1,50 +1,70 @@
-import omit from 'lodash.omit';
-import assignIn from 'lodash.assignin';
+const collatableTypes = ['rtpmap', 'fmtp', 'rtcp-fb'];
 
-const collateFmtp = (payload, attr) => {
-  if (!payload.params) {
-    payload.params = attr.params;
-  } else {
-    payload.params = payload.params.concat(attr.params);
+
+/**
+ * Merges a single attribute into an existing dictionary. This is basically
+ * just copying enumerable properties from attr to payload (like Object.assign)
+ * except that rather than overwriting existing keys it will allow multiple values
+ * to be under a single key (it's a multimap).
+ *
+ * @example:
+ *   const payload = { foo: 'bar' };
+ *   collateAttr(payload, {
+ *     id: 'foos',
+ *     fo: 'baz'
+ *   });
+ *   payload === {
+ *     id: 'foos',
+ *     foo: ['bar', 'baz'],
+ *   }
+ *
+ * @param  {Object} payload [description]
+ * @param  {Object} attr    [description]
+ * @return {[type]}         [description]
+ */
+export const collateAttr = (payload, attr) => {
+  for (const key in attr) {
+    if (attr.hasOwnProperty(key)) {
+      const value = attr[key];
+
+      if (!payload.hasOwnProperty(key)) {
+        payload[key] = value;
+      } else if (!Array.isArray(payload[key])) {
+        payload[key] = [payload[key], value];
+      } else {
+        payload[key].push(value);
+      }
+    }
   }
 };
 
-const collateRtcpFeedback = (payload, attr) => {
-  if (!payload.feedback) {
-    payload.feedback = [attr.feedback];
-  } else {
-    payload.feedback.push(attr.feedback);
-  }
-};
-
+/**
+ * Takes a list of supported formats (payload ids) and a list of attributes and
+ * produces an object that maps formats to their collated rtp, fmtp, and rtcp-fb
+ * attributes.
+ *
+ * This is usually used in the context of a specific SDP media line. In this
+ * context the formats will be the supported formats from the media line itself
+ * and attrs will be all attribute lines that belong to that media.
+ *
+ * @param  {Array} rawFormats Array of supported formats (payload ids)
+ * @param  {Array} attrs      Array of
+ * @return {Object}           A dictionary that maps format to payload properties.
+ */
 export const collatePayloads = (rawFormats, attrs) => {
   const formats = new Set(rawFormats);
   const payloads = {};
 
   for (const attr of attrs) {
-    const id = attr.format;
+    if (attr.value && attr.value.format && formats.has(attr.value.format)) {
+      const { format, ...rest } = attr.value;
 
-    if (id && formats.has(id)) {
-      if (!payloads.hasOwnProperty(id)) {
-        payloads[id] = { id };
+      if (!payloads.hasOwnProperty(format)) {
+        payloads[format] = { id: format };
       }
-      const payload = payloads[id];
 
-      switch (attr.type) {
-      case 'rtpmap':
-        assignIn(payloads[id], omit(attr, ['type', 'format']));
-        break;
-
-      case 'fmtp':
-        collateFmtp(payload, attr);
-        break;
-
-      case 'rtcp-fb':
-        collateRtcpFeedback(payload, attr);
-        break;
-
-      default:
-        // NOOP
+      if (collatableTypes.includes(attr.type)) {
+        collateAttr(payloads[format], rest);
       }
     }
   }
---
+++ test/unit/payloads_spec.js
@@ -0,0 +1,136 @@
+/* eslint max-nested-callbacks: [0] */
+import { collatePayloads, collateAttr } from '../../src/payloads.js';
+import { readAsset } from '../helpers/file_helpers.js';
+
+describe('Payloads', function() {
+  const rtcpFbs = [
+    {
+      feedback: {
+        type: 'ccm',
+        params: ['fir'],
+      },
+    }, {
+      feedback: {
+        type: 'nack',
+      },
+    }, {
+      feedback: {
+        type: 'nack',
+        params: ['pli'],
+      },
+    },
+  ];
+
+  describe('collateAttr', function() {
+    let payload;
+
+    beforeEach(function() {
+      payload = {};
+      collateAttr(payload, rtcpFbs[0]);
+    });
+
+    it('adds new keys when they are not already in the payload', function() {
+      // None of the keys in rtcpFbs exist in payload, so payload will basically
+      // just be a clone of rtcpFbs.
+      expect(payload).to.eql(rtcpFbs[0]);
+    });
+
+    it('converts existing non-array values to arrays', function() {
+      expect(payload.feedback).to.eql(rtcpFbs[0].feedback);
+      collateAttr(payload, rtcpFbs[1]);
+      expect(payload.feedback).to.eql([
+        rtcpFbs[0].feedback,
+        rtcpFbs[1].feedback,
+      ]);
+    });
+
+    it('pushes new values into existing array values', function() {
+      collateAttr(payload, rtcpFbs[1]);
+      collateAttr(payload, rtcpFbs[2]);
+
+      expect(payload.feedback).to.eql([
+        rtcpFbs[0].feedback,
+        rtcpFbs[1].feedback,
+        rtcpFbs[2].feedback,
+      ]);
+    });
+  });
+
+  describe('collatePayloads', function() {
+    let payloads, attrs, formats;
+
+    beforeEach(function() {
+      return readAsset('test_sdp.json').then((sdpJSONText) => {
+        const sdpJson = JSON.parse(sdpJSONText);
+        attrs = sdpJson.media[1].attrs;
+        formats = sdpJson.media[1].formats;
+        payloads = collatePayloads(formats, attrs);
+      });
+    });
+
+    function verifyRtpmap(id, encodingName, clockRate, encodingParams) {
+      const payload = payloads[id];
+      expect(payload).to.have.property('id', id);
+      expect(payload).to.have.property('encodingName', encodingName);
+      expect(payload).to.have.property('clockRate', clockRate);
+
+      if (encodingParams) {
+        expect(payload).to.have.property('encodingParams', encodingParams);
+      } else {
+        expect(payload.encodingParams).to.be.undefined;
+      }
+    }
+
+    it('merges the rtpmap details into the payload', function() {
+      verifyRtpmap('96', 'rtx', 90000);
+      verifyRtpmap('97', 'rtx', 90000);
+      verifyRtpmap('98', 'rtx', 90000);
+      verifyRtpmap('100', 'VP8', 90000);
+      verifyRtpmap('101', 'VP9', 90000);
+      verifyRtpmap('116', 'red', 90000);
+      verifyRtpmap('117', 'ulpfec', 90000);
+    });
+
+    function verifyFmtp(id, params) {
+      const payload = payloads[id];
+      expect(payload.params).to.eql(params);
+    }
+
+    it('merges the fmtp details into the payload', function() {
+      verifyFmtp('96', ['apt=100']);
+      verifyFmtp('97', ['apt=101']);
+      verifyFmtp('98', ['apt=116']);
+      verifyFmtp('100');
+      verifyFmtp('101');
+      verifyFmtp('116');
+      verifyFmtp('117');
+    });
+
+    function verifyRtcpFb(id, feedback) {
+      const payload = payloads[id];
+      expect(payload.feedback).to.eql(feedback);
+    }
+
+    it('merges the rtcp-fb details into the payload', function() {
+      verifyRtcpFb('96');
+      verifyRtcpFb('97');
+      verifyRtcpFb('98');
+      verifyRtcpFb('100', [
+        { type: 'ccm', params: ['fir'] },
+        { type: 'nack' },
+        { type: 'nack', params: ['pli'] },
+        { type: 'goog-remb' },
+        { type: 'transport-cc' },
+      ]);
+      verifyRtcpFb('101', [
+        { type: 'ccm', params: ['fir'] },
+        { type: 'nack' },
+        { type: 'nack', params: ['pli'] },
+        { type: 'goog-remb' },
+        { type: 'transport-cc' },
+      ]);
+      verifyRtcpFb('116');
+      verifyRtcpFb('117');
+    });
+  });
+});
